7c39f1b4ddd2a2e8e9d9ec7fc4fbc3998bba8d80
Ioannis Cherouvim <743305+cherouvim@users.noreply.github.com>
2024-02-02 08:54:08 +0200

    chore: Removal of non needed JS libs.

--- js/main.js
+++ js/main.js
@@ -5,98 +5,10 @@
 */
 
 $(document).ready(function () {
-	/**
-	 * Author: Heather Corey
-	 * jQuery Simple Parallax Plugin
-	 *
-	 */
-
-	(function ($) {
-		$.fn.parallax = function (options) {
-			var windowHeight = $(window).height();
-
-			// Establish default settings
-			var settings = $.extend(
-				{
-					speed: 0.15
-				},
-				options
-			);
-
-			// Iterate over each object in collection
-			return this.each(function () {
-				// Save a reference to the element
-				var $this = $(this);
-
-				// Set up Scroll Handler
-				$(document).scroll(function () {
-					var scrollTop = $(window).scrollTop();
-					var offset = $this.offset().top;
-					var height = $this.outerHeight();
-
-					// Check if above or below viewport
-					if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
-						return;
-					}
-
-					var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
-
-					// Apply the Y Background Position to Set the Parallax Effect
-					$this.css("background-position", "center " + yBgPosition + "px");
-				});
-			});
-		};
-	})(jQuery);
-
+	
 	//Loader
 	$(window).load(function () {
 		$(".loader-overlay").fadeOut("slow");
-	});
-
-	//Counter
-	$(".counter").counterUp({
-		delay: 10,
-		time: 1000
-	});
-
-	$("a[data-rel^=lightcase]").lightcase();
-
-	// Instantiate MixItUp
-	$(".portfolio-items").mixItUp({
-		animation: {
-			duration: 300
-		}
-	});
-
-	// Carousels
-	$(".cl-client-carousel").owlCarousel({
-		pagination: true,
-		slideSpeed: 300,
-		paginationSpeed: 400,
-		singleItem: true,
-		autoPlay: true
-	});
-
-	$(".cl-logo-carousel").owlCarousel({
-		items: 6,
-		itemsDesktop: [1199, 5],
-		itemsDesktopSmall: [979, 3],
-		stopOnHover: true,
-		autoPlay: 3000
-	});
-
-	$(".header-carousel").owlCarousel({
-		pagination: true,
-		navigation: true, // Show next and prev buttons
-		slideSpeed: 500,
-		paginationSpeed: 500,
-		singleItem: true,
-		autoPlay: true
-	});
-
-	// Parallax
-	$(".parallax-section").parallax({
-		speed: 0.1
 	});
 
 	// Header Changer on Scroll
